4f3df3661c145966e14243fc6cd4149d3dbef8e8
Manfred7 <manfred7@yandex.ru>
2021-04-24 12:29:20 +0300

    Разбивка на отдельные компоненты <Header/> <Navbar/> <Profile/>

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
 
-                <img
-                    src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_2.png"
+            <Header/>
+            <Navbar/>
+            <Profile/>
 
-                />
-
-            </header>
-
-            <nav className="nav">
-                <div><a href="#">Profile</a></div>
-                <div><a href="#">Messages</a></div>
-                <div><a href="#">News</a></div>
-                <div><a href="#">Music</a></div>
-                <div><a href="#">Settings</a></div>
-            </nav>
-            <div className="content">
-                <div>
-                    <img src="https://cdn.shazoo.ru/c800x360/161743_5LpKjDRLyv_4790632_samurai_jack_2422x1500_w.jpg"/>
-                </div>
-
-                <div>ava+description</div>
-                <div>mypost
-                    <div> new post</div>
-                </div>
-                <div>
-                    <div>post1</div>
-                    <div>post2</div>
-                    <div>post3</div>
-                    <div>post4</div>
-
-                </div>
-
-            </div>
         </div>
     );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://cdn.shazoo.ru/c800x360/161743_5LpKjDRLyv_4790632_samurai_jack_2422x1500_w.jpg"/>
+            </div>
+
+            <div>ava+description :)</div>
+            <div>mypost
+                <div> new post</div>
+            </div>
+            <div>
+                <div>post1</div>
+                <div>post2</div>
+                <div>post3</div>
+                <div>post4</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
