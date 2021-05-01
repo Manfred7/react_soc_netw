921019c49c3120997aee2dad1483da20baf221f0
Manfred7 <manfred7@yandex.ru>
2021-05-01 17:37:23 +0300

    Выносим css по отдельным модулям и подключаем

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://cdn.shazoo.ru/c800x360/161743_5LpKjDRLyv_4790632_samurai_jack_2422x1500_w.jpg"/>
             </div>
