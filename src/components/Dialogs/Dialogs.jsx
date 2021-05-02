50390389e2e9007dd9c50ae79d14fe170b0d4f07
Manfred7 <manfred7@yandex.ru>
2021-05-02 10:51:41 +0300

    Базовая верстка странички диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,28 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs}>
+
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are You</div>
+                <div className={s.message}>Fine</div>
+            </div>
         </div>
     );
 };
