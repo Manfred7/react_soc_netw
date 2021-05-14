22df9fa6d4f7edcf31a1b7b7240311f87fe340a6
Manfred7 <manfred7@yandex.ru>
2021-05-14 10:36:10 +0300

    загрузка по кнопке

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,42 +5,49 @@ import ava from '../../Assets/images/users_default_avatar.png'
 
 const Users = (props) => {
 
-    if (props.usersData.length === 0) {
-        const baseUrl ='https://social-network.samuraijs.com/api/1.0';
-        axios.get(baseUrl+'/users').then( response=>{
-            props.setUsers(response.data.items)
-        })
+    const getUsers = () => {
+        if (props.usersData.length === 0) {
+            const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
+            axios.get(baseUrl + '/users').then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>
+                get users
+            </button>
             {props.usersData.map((u) => {
 
                 return (
 
-                    <div key={u.id} className={s.users}>
-                        {u.name}
-                        <div>
 
-                            <img className={s.ava} src={
-                                u.photos.small!=null? u.photos.small: ava}
-                            />
+                <div key={u.id} className={s.users}>
+                    {u.name}
+                    <div>
 
-                        </div>
-                        <div>{u.id}</div>
-                        <div>
-                            {u.followed ?
-                                <button onClick={() => {
-                                    props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    props.follow(u.id)
-                                }}>follow</button>}
+                        <img className={s.ava} src={
+                            u.photos.small != null ? u.photos.small : ava}
+                        />
 
-                        </div>
-                        <div>{u.status}</div>
+                    </div>
+                    <div>{u.id}</div>
+                    <div>
+                        {u.followed ?
+                            <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>follow</button>}
 
-                    </div>)
+                    </div>
+                    <div>{u.status}</div>
+
+                </div>
+            )
             })}
         </div>
     );
