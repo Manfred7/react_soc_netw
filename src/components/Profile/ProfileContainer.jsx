8121a609b3dbd8df26a77362edf3832502165293
Manfred7 <manfred7@yandex.ru>
2021-05-30 11:11:25 +0300

    withRouter, props.match.params 1. Добавили в роут параметр :userId <Route path="/profile/:userId" render={()=><ProfileContainer  />} /> 2. Обернули нашу контейнерную компоненту в withRouter 3. в componentDidMount извлекаем id пользователя из адресной строки через let currentProfileId = this.props.match.params.userId;
    Попадает в адресную строку в Users через <NavLink to={'/profile/'+u.id}>

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,31 +3,32 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile_reducer";
+import {withRouter} from "react-router-dom"
 
 class ProfileContainer extends Component {
     componentDidMount() {
+        let currentProfileId = this.props.match.params.userId;
         const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
-        //axios.get(baseUrl + `/profile/${this.props.profileId}`)
-        axios.get(baseUrl + `/profile/2`)
+        axios.get(baseUrl + `/profile/${currentProfileId}`)
             .then(response => {
                 this.props.setUserProfile(response.data);
-    //            this.props.setTotalUsersCount(response.data.totalCount);
-    //            this.props.togleIsFetching(false);
             })
     }
 
     render() {
         return (
             <div>
-                <Profile {...this.props} currentProfile ={this.props.currentProfile} />
+                <Profile {...this.props} currentProfile={this.props.currentProfile}/>
             </div>
         );
     }
 }
 
-let mapStateToProps =(state)=>{
+let mapStateToProps = (state) => {
     return {
-        currentProfile : state.profilePage.currentProfile
+        currentProfile: state.profilePage.currentProfile
     }
 }
-export default connect(mapStateToProps, {setUserProfile}) (ProfileContainer);
+
+let withUrlDataContainerComponent = withRouter(ProfileContainer)
+export default connect(mapStateToProps, {setUserProfile})(withUrlDataContainerComponent);
